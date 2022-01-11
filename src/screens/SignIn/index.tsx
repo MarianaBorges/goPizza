989dffe2de768cc8fcb319573b5e4b484141c4e0
MaryBorges <marianamarianoborges>
2022-01-11 15:16:53 -0300

    setup, input component, login screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Input } from "@components/Input";
+
+import { Container } from "./styles";
+
+export function SignIn(){
+    return (
+        <Container>
+            <Input 
+                placeholder="E-mail"
+                type="secundary"
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
+            <Input 
+                placeholder="Senha"
+                type="secundary"
+                secureTextEntry
+            />
+        </Container>
+    )
+}
