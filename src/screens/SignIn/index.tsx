4c9f7b63d4aad34762622927e208118d169ce2a8
MaryBorges <marianamarianoborges>
2022-01-12 09:53:17 -0300

    add button component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 import { Container } from "./styles";
 
 export function SignIn(){
     return (
         <Container>
+
             <Input 
                 placeholder="E-mail"
                 type="secundary"
                 autoCorrect={false}
                 autoCapitalize="none"
             />
+
             <Input 
                 placeholder="Senha"
                 type="secundary"
                 secureTextEntry
             />
+
+            <Button
+                title="Entrar"
+                type="secundary"
+            />
+
         </Container>
     )
 }
