fb6b30e51d7876c01b999ca677dd3ecaeaf0c1ef
MaryBorges <marianamarianoborges>
2022-01-14 10:59:37 -0300

    Photo  component

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Platform, TouchableOpacity } from "react-native";
 
 import { ButtonBack } from "@components/ButtonBack";
+import { Photo } from "@components/Photo";
 
 import { Container, Header, Title, DeleteLabel} from "./styles";
 
@@ -9,12 +10,15 @@ export function Product(){
     return(
         <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
             <Header>
-                <ButtonBack />
+                <ButtonBack /> 
+
                 <Title>Cadastrar</Title>
+
                 <TouchableOpacity>
                     <DeleteLabel>Deletar</DeleteLabel>
                 </TouchableOpacity>
             </Header>
+            <Photo uri={null}/>
         </Container>
     )
 }
